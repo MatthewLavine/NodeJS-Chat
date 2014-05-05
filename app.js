87cf3459bc75e626a3b696c25ad93b81e4875040
Matthew Lavine <hermitlavine@gmail.com>
2014-05-05 15:26:28 -0400

    Extended Express scope

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ io.set('log level', 1);
 app.set("view options", {layout: false});
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
     res.render('index.ejs', {
     	ip: ip
         });
