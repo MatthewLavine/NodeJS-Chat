1bdf78f03ab974d2bf0a86378de2d6fb4ae8df2b
Matthew Lavine <hermitlavine@gmail.com>
2014-05-07 12:02:29 -0400

    Merged hammerConfig into app

--- public/js/app.js
+++ public/js/app.js
@@ -258,3 +258,11 @@ $('#toggleSounds').click(function(){
     playSound(alert);
   }
 });
+
+var swipeleft = Hammer(document.body).on("swiperight", function(ev) {
+  $(".off-canvas-wrap").addClass("move-right");
+});
+
+var swiperight = Hammer(document.body).on("swipeleft", function(ev) {
+  $(".off-canvas-wrap").addClass("move-left");
+});
--- public/js/hammerConfig.js
+++
@@ -1,8 +0,0 @@
-  var swipeleft = Hammer(document.body).on("swiperight", function(ev) {
-    $(".off-canvas-wrap").addClass("move-right");
-  });
-
-  var swiperight = Hammer(document.body).on("swipeleft", function(ev) {
-    $(".off-canvas-wrap").addClass("move-left");
-  });
-  
