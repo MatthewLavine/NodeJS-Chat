8814f629346928a719c856642922daea8f0589d6
Matthew Lavine <hermitlavine@gmail.com>
2014-05-07 12:01:59 -0400

    Added grunt csslint and min

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,16 +24,36 @@ module.exports = function(grunt) {
           multistr: true
       }
     },
+    cssmin: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'public/css/app.min.css': ['public/css/app.css']
+        }
+      }
+    },
+    csslint: {
+      src: ['public/css/app.css'],
+      options: {
+        "important": false,
+        "box-model": false,
+        "known-properties": false
+      }
+    },
     watch: {
-      files: ['public/js/app.js'],
-      tasks: ['jshint','uglify']
+      files: ['public/js/app.js', 'public/css/app.css'],
+      tasks: ['uglify', 'jshint', 'cssmin', 'csslint']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
 
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'jshint', 'cssmin', 'csslint']);
 
 };
