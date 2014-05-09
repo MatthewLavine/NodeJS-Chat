631c80ca3fd3f432eb33912c9a1e731164e42c85
Matthew Lavine <hermitlavine@gmail.com>
2014-05-09 00:30:10 -0400

    Fixing missing var

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.post('/gitpull', function(req, res) {
   function puts(error, stdout, stderr) {sys.puts(stdout)}
   exec("git reset --hard HEAD", puts);
   exec("git pull", puts);
+  var arr = req.body.commits[0].modified;
   if((arr.join(',').indexOf("app.min.js") > -1) || (arr.join(',').indexOf("app.min.css") > -1)){
     io.sockets.emit('annouce', {message : '<span class="adminMessage">SYSTEM UPDATE COMPLETE, BROWSER RELOAD IS NECCESARY.</span>'});
   } else {
